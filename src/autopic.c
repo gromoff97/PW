b107e5484c070949a6f537243e4dddc5c7d2d2af
gromoff97 <gromoff97@mail.ru>
2017-11-15 08:59:48 +0300

    macro was defined and simple foo for CheckArgs was created

--- src/autopic.c
+++ src/autopic.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+#define REQARGC 3
+
 int CheckArgs(int);
 
 int main(int argc, char const *argv[])
 {
+	if (!CheckArgs(argc)) return 1;
+	return 0;
+}
+
+int CheckArgs(int argcount)
+{
+	if (argcount != REQARGC)
+	{
+		if (argcount < REQARGC)
+			printf("Bad. Not enough %d argument(-s) to make it work.\n",(REQARGC-argcount));
+		else
+			printf("Bad. You should remove %d argument(-s) to make it work.\n",(argcount-REQARGC));
+		return 1;
+	}
 	return 0;
 }
