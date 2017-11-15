a62e3d388910bba3f8bbf1ac5b808f2b2a868e5d
gromoff97 <gromoff97@mail.ru>
2017-11-15 09:05:52 +0300

    foo was beautified

--- src/autopic.c
+++ src/autopic.c
@@ -14,10 +14,8 @@ int AreArgsValid(int argcount)
 {
 	if (argcount != REQARGC)
 	{
-		if (argcount < REQARGC)
-			printf("Bad. Not enough %d argument(-s) to make it work.\n",(REQARGC-argcount));
-		else
-			printf("Bad. You should remove %d argument(-s) to make it work.\n",(argcount-REQARGC));
+		if (argcount < REQARGC) printf("Bad. Not enough %d argument(-s) to make it work.\nClosing\n",(REQARGC-argcount));
+		else printf("Bad. You should remove %d argument(-s) to make it work.\nClosing\n",(argcount-REQARGC));
 		return 1;
 	}
 	return 0;
