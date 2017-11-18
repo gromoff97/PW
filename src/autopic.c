c62ab10cf444bc549ce158ac5e5cc7098805090b
gromoff97 <gromoff97@mail.ru>
2017-11-18 10:00:42 +0300

    define-code was moved to header-file & custom header was included

--- src/autopic.c
+++ src/autopic.c
@@ -4,22 +4,8 @@
 #include <stdbool.h>
 #include <libgen.h>
 #include <string.h>
+#include <argcheck.h>
 
-#define REQARGC 3
-
-typedef enum {
-	CHECK_OK = 0,
-	CHECK_INVALID_ARG_COUNT,
-	CHECK_DIR_NOT_EXISTS,
-	CHECK_DIR_NOT_EMPTY,
-	CHECK_DIRS_NAMES_NOT_UNIQUE
-} CheckArgErrCode;
-
-CheckArgErrCode AreArgsValid(int,char**);
-	static bool IsArgCountValid(int);
-	static bool IsDirExists(char*);
-	static bool IsDirEmpty(char*);
-	static bool AreDirNamesUnique(char**);
 
 int main(int argc, char** argv)
 {
