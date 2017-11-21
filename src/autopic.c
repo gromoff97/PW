a7e95f5c4d4953fc2c5e942011bd08f4d097fb19
gromoff97 <gromoff97@mail.ru>
2017-11-21 05:11:58 +0300

    macro val was changed; num of params of startscript-foo was changed

--- src/autopic.c
+++ src/autopic.c
@@ -1,6 +1,6 @@
 #include <argcheck.h>
 
-#define SH_PATH "src/defpic.sh"
+#define SH_PATH "./src/defpic.sh"
 
 typedef enum {
 	SCRIPT_OK = 0,
@@ -8,18 +8,18 @@ typedef enum {
 	SCRIPT_BAD
 } ScriptErrCode;
 
-ScriptErrCode StartScript(char**);
+ScriptErrCode StartScript(int,char**);
 	static bool DoesScriptExist();
 	static int ExecuteMainScript();
 
 int main(int argc, char* argv[])
 {
 	if (AreArgsValid(argc,argv) != CHECK_OK) return 1;
-	if (StartScript(argv) != SCRIPT_OK) return 2;
+	if (StartScript(argc,argv) != SCRIPT_OK) return 2;
 	return 0;
 }
 
-ScriptErrCode StartScript(char** arg_buf)
+ScriptErrCode StartScript(int arg_count,char** arg_buf)
 {
 	if ( false == DoesScriptExist() ) {printf("Execute script does not exist.Closing.\n");return SCRIPT_BAD_NOT_EXIST;}
 	if ( 0 != ExecuteMainScript() ) {printf("Script failure. Closing.\n");return SCRIPT_BAD;}
@@ -35,5 +35,5 @@ static bool DoesScriptExist()
 
 static int ExecuteMainScript()
 {
-	return 0;
+	return system(SH_PATH);
 }
