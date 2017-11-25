3ed8ac239a44576ad8c3e7e251dda57b92cf9d42
gromoff97 <gromoff97@mail.ru>
2017-11-25 05:56:47 +0300

    checking dir was implemented

--- src/SH/defpic.sh
+++ src/SH/defpic.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+tmpdir=".tmp_dir" 
+
+if [ ! -d "$tmpdir" ]; then
+	mkdir $tmpdir
+fi
+
 pic_name=$1
 watch_dir_name=$2
 shift
 shift
 
-for outdir 
+for outdir
 do 
- #do smth	
+  echo $outdir
 done
 
 exit 0;
