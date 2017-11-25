2439121fc120f1d4285f1ee9a8fee05b6dd4c0d1
gromoff97 <gromoff97@mail.ru>
2017-11-25 11:21:28 +0300

    macroses were moved

--- include/watcher.h
+++ include/watcher.h
@@ -1,6 +1,12 @@
 #ifndef _WATCHER_H_
 #define _WATCHER_H_
 
+#define SH_PATH "./src/SH/defpic.sh"
+#define SHEBANG_RAW "#!/bin/bash\n"
+#define COMMAND_BUF_SIZE ( 256 )
+#define STOP_FILE_NAME ".end_inotify_properly"
+#define EVENT_SIZE ( sizeof (struct inotify_event) )
+#define EVENT_BUF_LEN ( 1024 * ( EVENT_SIZE + 16 ) )
 
 typedef enum {
 	SCRIPT_OK = 0,
--- src/C/autopic.c
+++ src/C/autopic.c
@@ -7,13 +7,6 @@
 #include <stdlib.h>
 #include <watcher.h>
 
-#define SH_PATH "./src/SH/defpic.sh"
-#define SHEBANG_RAW "#!/bin/bash\n"
-#define COMMAND_BUF_SIZE ( 256 )
-#define STOP_FILE_NAME ".end_inotify_properly"
-#define EVENT_SIZE ( sizeof (struct inotify_event) )
-#define EVENT_BUF_LEN ( 1024 * ( EVENT_SIZE + 16 ) )
-
 	static bool DoesScriptExist();
 		static bool DoesShebangExist(FILE*);
 	static int ExecuteWatchLoop(int,char*[]);
