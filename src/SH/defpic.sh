7be91fb79c3374f477a3c0d7b7858f931f8370ba
gromoff97 <gromoff97@mail.ru>
2017-11-25 10:55:12 +0300

    first "night" release of script was created

--- src/SH/defpic.sh
+++ src/SH/defpic.sh
@@ -1,18 +1,66 @@
-#!/bin/sh
-tmpdir=".tmp_dir" 
+#!/bin/bash
 
+if [ "$#" -lt 3 ]; then
+    echo "Illegal number of parameters"
+    exit 1;
+fi
+
+tmpdir=".tmp_dir"
+pic_name=$1
+pic_name_no_ext=${pic_name:0:-4}
+watch_dir_name=$2
+
+#---------CHECKING TMP_DIR---------- 
 if [ ! -d "$tmpdir" ]; then
 	mkdir $tmpdir
 fi
+#------CHECKING TMP_DIR (END)-------
 
-pic_name=$1
-watch_dir_name=$2
+#----------------CHECKING WATCHDIR------------
+if [[ $watch_dir_name =~ ^$tmpdir(\/)?$ ]]; then
+	echo "watch directory can't be named like that.Closing"
+	exit 2
+fi
+if [ ! -d "$watch_dir_name" ]; then
+	echo "Dat dir does not exist.Closing"
+	exit 3
+fi
+if [[ ! $watch_dir_name =~ ^.*\/$ ]]; then
+	watch_dir_name="$watch_dir_name/"
+fi
+#-----------CHECKING WATCHDIR (END)-----------
+
+pic_full_path="$watch_dir_name$pic_name"
+
+#---------CHECKING PICTURE----------
+if [ ! -f $pic_full_path ]; then
+    echo "picture \"$pic_full_path\" not found.Closing"
+    exit 4
+fi
+#-----CHECKING PICTURE (END)-------
+
+mv $pic_full_path $tmpdir
 shift
 shift
+
+#----------MAIN PART-------------
+mogrify -format png $tmpdir/$pic_name || exit 5
+pngquant -f --ext .png --quality 10 $tmpdir/$pic_name_no_ext.png || exit 6
+#----------MAIN PART (END)-------
 
 for outdir
 do 
-  echo $outdir
+	if [ "$outdir" == "watch_dir_name" ]; then
+		echo "output dir can't be watch dir. Closing"
+	fi
+	if [ ! -d "$outdir" ]; then
+		mkdir -p $outdir
+		echo "$outdir was created."
+	fi
+cp $tmpdir/$pic_name_no_ext.png $outdir
 done
 
-exit 0;
+rm -r $tmpdir/$pic_name
+rm -r $tmpdir/$pic_name_no_ext.png
+
+exit 0
