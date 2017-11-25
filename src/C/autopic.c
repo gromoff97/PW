874aa671ea2e578c6d9f26982299ffa4497c32e4
gromoff97 <gromoff97@mail.ru>
2017-11-25 11:28:18 +0300

    prototypes were moved

--- src/C/autopic.c
+++ src/C/autopic.c
@@ -7,13 +7,6 @@
 #include <stdlib.h>
 #include <watcher.h>
 
-	static bool DoesScriptExist();
-		static bool DoesShebangExist(FILE*);
-	static int ExecuteWatchLoop(int,char*[]);
-		static bool IsPic(char[]);
-		static int DelFile(char*,char*);
-		static bool IsStopFile(char*);
-		static int ExecuteShScript(int,char*[],char *);
 
 int main(int argc, char* argv[])
 {
---
+++ src/C/watcher.c
@@ -0,0 +1,7 @@
+	static bool DoesScriptExist();
+		static bool DoesShebangExist(FILE*);
+	static int ExecuteWatchLoop(int,char*[]);
+		static bool IsPic(char[]);
+		static int DelFile(char*,char*);
+		static bool IsStopFile(char*);
+		static int ExecuteShScript(int,char*[],char *);
