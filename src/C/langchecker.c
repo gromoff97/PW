5d3350fd3ebae6ed5367edbb8f7a61b98ac149b6
gromoff97 <gromoff97@mail.ru>
2017-12-20 12:40:27 +0300

    night-version of strengl-foo was created

--- src/C/langchecker.c
+++ src/C/langchecker.c
@@ -1,6 +1,13 @@
 #include <langchecker.h>
+#include <string.h>
 
-LangErrCode strengl( char * buffer )
+static const char eng_buf[]={".~/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+
+LangErrCode strengl( char buffer[] )
 {
+	for (size_t buf_counter = 0; buf_counter < strlen(buffer); buf_counter++)
+			if ( NULL == strchr(eng_buf,buffer[buf_counter]) )
+				return CHECK_LANG_NON_ENGL;
+			
 	return CHECK_LANG_OK;
 }
