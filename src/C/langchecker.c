63e0c317cb39a4efa54e1216f8737a132aa6b61a
gromoff97 <gromoff97@mail.ru>
2018-01-03 07:25:30 +0300

    space-symbol and nums were added in eng-buf

--- src/C/langchecker.c
+++ src/C/langchecker.c
@@ -1,12 +1,12 @@
 #include <langchecker.h>
 #include <string.h>
 
-static const char eng_buf[]={".~/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+static const char eng_buf[]={" .~/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"};
 
 LangErrCode strengl( char buffer[] )
 {
-	for (size_t buf_counter = 0; buf_counter < strlen(buffer); buf_counter++)
-			if ( NULL == strchr(eng_buf,buffer[buf_counter]) )
+	for (size_t buf_counter = 0; buf_counter < strlen( buffer ); buf_counter++)
+			if ( NULL == strchr( eng_buf,buffer[ buf_counter ] ) )
 				return CHECK_LANG_NON_ENGL;
 			
 	return CHECK_LANG_OK;
